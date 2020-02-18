120bae2bf9e3ebc9d4b3a7be8bf5d84554cf290e
Karim Macknojia <alishahal@msn.com>
2020-02-18 01:22:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ['dog', 'cat', 100, 3.1415].push()
+  return array.push('Eagle')
 end
 
 def add_element_to_start_of_array(array, element)
